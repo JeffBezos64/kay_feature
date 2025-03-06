d0e6a8195ac2af9c8149028cb70cdde01706cf72
James Kay <jka87@uclive.ac.nz>
2025-03-07 12:22:39 +1300

    changes

--- kay_feature/feature_extractor.py
+++ kay_feature/feature_extractor.py
@@ -11,21 +11,19 @@ class TfidfEmbeddingVectorizer(object):
     def __init__(self, word2vec):
         self.word2vec = word2vec
         self.word2weight = None
-        self.dim = len(word2vec.itervalues().next())
+        self.dim = len(word2vec['the'])
 
     def fit(self, X, y):
         tfidf = TfidfVectorizer(analyzer=lambda x: x)
         tfidf.fit(X)
-        # if a word was never seen - it must be at least as infrequent
-        # as any of the known words - so the default idf is the max of 
-        # known idf's
         max_idf = max(tfidf.idf_)
         self.word2weight = defaultdict(
             lambda: max_idf,
             [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
 
         return self
-
+#TODO change this from mean to just scaling the tf-idf values.
+#TODO or alternatively test these features
     def transform(self, X):
         return np.array([
                 np.mean([self.word2vec[w] * self.word2weight[w]
@@ -33,6 +31,3 @@ class TfidfEmbeddingVectorizer(object):
                         [np.zeros(self.dim)], axis=0)
                 for words in X
             ])
-
-
-class TfidfEmbeddingVectorizer(object)
