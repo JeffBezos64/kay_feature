6e5d10a6edd08bf81ff408a3c7cfe4102239eafc
James Kay <jka87@uclive.ac.nz>
2025-03-07 12:40:13 +1300

    Still features to update but the skeleton code is there

--- kay_feature/feature_extractor.py
+++ kay_feature/feature_extractor.py
@@ -7,11 +7,35 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 
 
 
-class TfidfEmbeddingVectorizer(object):
-    def __init__(self, word2vec):
-        self.word2vec = word2vec
+class NonGenSimMeanTfidfEmbeddingVectorizer(object):
+    def __init__(self, embedder):
+        self.embedder = embedder
         self.word2weight = None
-        self.dim = len(word2vec['the'])
+        self.dim = len(self.embedder.vec('the'))
+
+    def fit(self, X, y):
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf.fit(X)
+        max_idf = max(tfidf.idf_)
+        self.word2weight = defaultdict(
+            lambda: max_idf,
+            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+
+        return self
+
+    def transform(self, X):
+        return np.array([
+                np.mean([self.embedder.vec(w) * self.word2weight[w]
+                         for w in words if w in self.word2vec] or
+                        [np.zeros(self.dim)], axis=0)
+                for words in X
+            ])
+
+class NonGenSimTfidfEmbeddingVectorizer(object):
+    def __init__(self, embedder):
+        self.embedder = embedder
+        self.word2weight = None
+        self.dim = len(self.embedder.vec('the'))
 
     def fit(self, X, y):
         tfidf = TfidfVectorizer(analyzer=lambda x: x)
@@ -26,8 +50,69 @@ class TfidfEmbeddingVectorizer(object):
 #TODO or alternatively test these features
     def transform(self, X):
         return np.array([
+                np.mean([self.embedder.vec(w) * self.word2weight[w]
+                         for w in words if w in self.word2vec] or
+                        [np.zeros(self.dim)], axis=0)
+                for words in X
+            ])
+
+class GenSimMeanTfidfEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        self.word2weight = None
+        if len(word2vec)>0:
+            self.dim=len(word2vec[next(iter(glove_small))])
+        else:
+            self.dim=0
+        
+    def fit(self, X, y):
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf.fit(X)
+        # if a word was never seen - it must be at least as infrequent
+        # as any of the known words - so the default idf is the max of 
+        # known idf's
+        max_idf = max(tfidf.idf_)
+        self.word2weight = defaultdict(
+            lambda: max_idf, 
+            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+    
+        return self
+    
+    def transform(self, X):
+        return np.array([
                 np.mean([self.word2vec[w] * self.word2weight[w]
                          for w in words if w in self.word2vec] or
                         [np.zeros(self.dim)], axis=0)
                 for words in X
             ])
+#TODO convert to not use the mean.
+class GenSimTfidfEmbeddingVectorizer(object):
+    def __init__(self, word2vec):
+        self.word2vec = word2vec
+        self.word2weight = None
+        if len(word2vec)>0:
+            self.dim=len(word2vec[next(iter(glove_small))])
+        else:
+            self.dim=0
+        
+    def fit(self, X, y):
+        tfidf = TfidfVectorizer(analyzer=lambda x: x)
+        tfidf.fit(X)
+        # if a word was never seen - it must be at least as infrequent
+        # as any of the known words - so the default idf is the max of 
+        # known idf's
+        max_idf = max(tfidf.idf_)
+        self.word2weight = defaultdict(
+            lambda: max_idf, 
+            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
+    
+        return self
+    
+    def transform(self, X):
+        return np.array([
+                np.mean([self.word2vec[w] * self.word2weight[w]
+                         for w in words if w in self.word2vec] or
+                        [np.zeros(self.dim)], axis=0)
+                for words in X
+            ])
+
