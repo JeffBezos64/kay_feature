21ea581c078954f7a03ec3a54ea6da7ab0e87f08
James Kay <jka87@uclive.ac.nz>
2025-03-07 17:47:30 +1300

    updated work

--- kay_feature/feature_extractor.py
+++ kay_feature/feature_extractor.py
@@ -1,8 +1,21 @@
+import string
+import difflib
+from collections import Counter
+from typing import List, Tuple
+from concurrent.futures import ThreadPoolExecutor
+
 import pandas as pd
 import numpy as np
 from gensim.models.word2vec import Word2Vec
 from collections import Counter, defaultdict
 from sklearn.feature_extraction.text import TfidfVectorizer
+
+from language_checkers.spell_checker import SpellChecker
+from language_checkers.grammar_checker import GrammarChecker
+from language_checkers.pos_tagger import POSTagger
+
+from tqdm import tqdm
+import nltk
 
 class NonGenSimMeanTfidfEmbeddingVectorizer(object):
     def __init__(self, embedder):
@@ -54,3 +67,39 @@ class GenSimMeanTfidfEmbeddingVectorizer(object):
                         [np.zeros(self.dim)], axis=0)
                 for words in X
             ])
+
+#, remove_stopwords=True make sure to include stopwords!
+class EmbeddingFeatureExtractor():
+    def __init__(self, vectorizer=None, spell_check_flag=True, max_edit_distance=2):
+        self._vectorizer = vectorizer
+        self.spell_check_flag = spell_check
+        self._spell_checker = SpellChecker(max_edit_distance=max_edit_distance)
+
+    def transform_spelling(self, X):
+        X = [[self._spell_checker.get_close_correction(z) for z in y] for y in X]
+        return X
+    
+    def spell_check(self, X):
+        if self.spell_check_flag == True:
+            return self.transform_spelling(self, X)
+        else:
+            return X
+
+    def preprocess(self, X):
+        X = [self._clean_text(x) for x in X]
+        X = [self._remove_punctuation(x) for x in X]
+        X = [nltk.word_tokenize(x) for x in X]
+        return X
+        
+    def _clean_text(self, text: str) -> str:
+        return text.replace('\n', ' ')
+
+    def _remove_punctuation(self, text: str) -> str:
+        return text.translate(str.maketrans('', '', string.punctuation))
+
+    def transform(self, X):
+        X = self.preprocess(X)
+        if spell_check_flag == True:
+            X = self.spell_check(X)
+        return self._vectorizer.transform(X)
+        
