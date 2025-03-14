a76f8da466897ba440d4869c6b3f00b8d8ce878f
James Kay <42159491+JeffBezos64@users.noreply.github.com>
2025-03-14 13:05:28 +1300

    removed unused code

--- kay_feature/feature_extractor.py
+++ kay_feature/feature_extractor.py
@@ -45,35 +45,6 @@ class NonGenSimMeanTfidfEmbeddingVectorizer(object):
         tfidf_data.eliminate_zeros()
         return tfidf_data
 
-
-class GenSimMeanTfidfEmbeddingVectorizer(object):
-    def __init__(self, word2vec):
-        self.word2vec = word2vec
-        self.word2weight = None
-        if len(word2vec)>0:
-            self.dim=len(word2vec[next(iter(glove_small))])
-        else:
-            self.dim=0
-        
-    def fit(self, X, y):
-        tfidf = TfidfVectorizer(analyzer=lambda x: x)
-        tfidf.fit(X)
-        max_idf = max(tfidf.idf_)
-        self.word2weight = defaultdict(
-            lambda: max_idf, 
-            [(w, tfidf.idf_[i]) for w, i in tfidf.vocabulary_.items()])
-    
-        return self
-    
-    def transform(self, X):
-        return np.array([
-                np.mean([self.word2vec[w] * self.word2weight[w]
-                         for w in words if w in self.word2vec] or
-                        [np.zeros(self.dim)], axis=0)
-                for words in X
-            ])
-
-
 class GensimEmbed(object):
     def __init__(self, model, OOVRandom=False):
         self.model = model
